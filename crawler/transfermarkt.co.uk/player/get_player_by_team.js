0c8361b8eaa06e64d1b4ec75384b276fca02a8c3
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-08-11 15:38:07 +0800

    get player by team will get the player trasfer in pnce

--- crawler/transfermarkt.co.uk/player/get_player_by_team.js
+++ crawler/transfermarkt.co.uk/player/get_player_by_team.js
@@ -2,7 +2,7 @@
  * @author nttdocomo
  */
 var http = require("http"), cheerio = require('cheerio'),connection = require("../db"), mysql = require('mysql'),StringDecoder = require('string_decoder').StringDecoder,Team = require('../team/model'),
-Player = require('./model'),
+Player = require('./model'),moment = require('moment'),
 trim = require('../utils').trim,
 pool  = require('../pool'),
 Crawler = require("simplecrawler"),
@@ -18,10 +18,9 @@ crawler.customHeaders = {
 crawler.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36';
 crawler.on("fetchcomplete",function(queueItem, responseBuffer, response){
     var decoder = new StringDecoder('utf8'),sql,
-    $ = cheerio.load(decoder.write(responseBuffer)),
-    team,player;
+    $ = cheerio.load(decoder.write(responseBuffer));
     if(/^\/\S+?\/startseite\/verein\/\d+?(\/saison_id\/\d{4})?$/i.test(queueItem.path)){
-	    team = new Team($);
+	    var team = new Team($);
 	    //team.save_team_player(pool)
 	    //team.update_team_player(pool)
 	    team.get_player_url().forEach(function(url){
@@ -33,8 +32,56 @@ crawler.on("fetchcomplete",function(queueItem, responseBuffer, response){
     	//team.save(pool);
     }
     if(/^\/\S+\/profil\/spieler\/\d{1,6}$/.test(queueItem.path)){
-	    player = new Player($);
+	    var player = new Player($),path = queueItem.path;
 	    player.save(pool);
+	    path = path.replace('profil','korrektur');
+    	crawler.queueURL(host + path);
+    }
+    if(/^\/\S+\/korrektur\/spieler\/\d{1,6}$/.test(queueItem.path)){
+    	var $ = cheerio.load(decoder.write(responseBuffer));
+		transfer_table = $('#transfers'),transfer_tbody = transfer_table.find('>tbody'),transfers_id = transfer_tbody.find(' > input[id$="trans_id"]');
+		transfers_id.each(function(index,el){
+			var $el = $(el),
+			id = $el.val(),
+			player_id = $('#submenue > li').eq(1).find('> a').attr('href').replace(/\S+?\/(\d{1,9})$/,'$1'),
+			season = $el.next().children().eq(0).find('select').val(),
+			transfer_date = $el.next().children().eq(3).find('input').val(),
+			month = $el.next().children().eq(4).find('select').val(),
+			loan = $el.next().children().eq(5).find('select').val(),
+			transfer_sum = $el.next().children().eq(7).find('input').val(),
+			contract_period = [$el.next().next().children().eq(0).find('input').eq(2).val(),$el.next().next().children().eq(0).find('input').eq(1).val(),$el.next().next().children().eq(0).find('input').eq(0).val()].join('-');
+			contract_period = /\d{4}\-\d{2}\-\d{2}/.test(contract_period) ? contract_period : '0000-00-00';
+			transfer_date = /\d{2}\.\d{2}\.\d{4}/.test(transfer_date) ? transfer_date.replace(/(\d{2})\.(\d{2})\.(\d{4})/,'$3-$2-$1') : moment(month + ' 1,' + season).format('YYYY-MM-DD');
+			transfer_sum = /\d/.test(transfer_sum) ? transfer_sum.replace(/\./g,'') : 0;
+			var sql = mysql.format("INSERT INTO transfermarket_transfer (id,season,transfer_date,transfer_sum,player_id,contract_period,loan) SELECT ? FROM dual WHERE NOT EXISTS(SELECT id FROM transfermarket_transfer WHERE id = ?)", [[id,season,transfer_date,transfer_sum,player_id,contract_period,loan],id]);
+			pool.getConnection(function(err, connection) {
+				connection.query(sql, function(err) {
+				    if (err) throw err;
+				    connection.release();
+				});
+			});
+		})
+	    var path = queueItem.path.replace('korrektur','transfers');
+    	crawler.queueURL(host + path);
+    };
+    if(/^\/\S+\/transfers\/spieler\/\d{1,6}$/.test(queueItem.path)){
+    	var $ = cheerio.load(decoder.write(responseBuffer)),
+		transfer_table = $('.responsive-table > table'),transfer_tbody = transfer_table.find('>tbody'),transfers_tr = transfer_tbody.find(' > tr');
+		transfers_tr.each(function(index,el){
+			var $el = $(el);
+			if(typeof($el.children().last().find('a').attr('href')) !== 'undefined'){
+				var id = $el.children().last().find('a').attr('href').replace(/\S+?\/(\d+)$/,'$1'),
+				releasing_team_id = $el.children().eq(2).find('a').attr('href').replace(/^\/\S+?\/transfers\/verein\/(\d+?)(\/\S+)?$/,'$1'),
+				taking_team_id = $el.children().eq(5).find('a').attr('href').replace(/^\/\S+?\/transfers\/verein\/(\d+?)(\/\S+)?$/,'$1');
+				var sql = mysql.format("UPDATE transfermarket_transfer SET ? WHERE id = ?", [{releasing_team_id:releasing_team_id,taking_team_id:taking_team_id},id]);
+				pool.getConnection(function(err, connection) {
+					connection.query(sql, function(err) {
+					    if (err) throw err;
+					    connection.release();
+					});
+				});
+			};
+		})
     }
 }).on('complete',function(){
 	console.log('complete');
