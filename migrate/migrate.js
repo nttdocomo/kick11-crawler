3d20e3be6d0220d53962081e567fc24f4df1bc7a
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-09-01 15:56:56 +0800

    add migrate js to do some migrate job

---
+++ migrate/migrate.js
@@ -0,0 +1,13 @@
+/**
+ * @author nttdocomo
+ */
+var http = require("http"), mysql = require('mysql'),
+pool  = require('../pool'),
+var sql = "INSERT INTO `player2position`(player_id,position_id) SELECT transfermarket_player.player_ref_id,position.id FROM `position` JOIN `transfermarket_player` ON transfermarket_player.position = position.name WHERE CONCAT(transfermarket_player.player_ref_id,'-',position.id) NOT IN (SELECT CONCAT(player_id,'-',position_id) FROM `player2position`) AND transfermarket_player.player_ref_id != 0";
+pool.getConnection(function(err, connection) {
+	connection.query(sql, function(err,result) {
+	    if (err) throw err;
+	    connection.release();
+	    console.log('deleted ' + result.affectedRows + ' rows');
+	});
+});
