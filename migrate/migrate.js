b5f8a6cbeff31588f4977e88fd66387a5954afd0
nttdocomo <nttdocomo.ouyi@gmail.com>
2014-09-01 16:01:40 +0800

    add migrate js to do some migrate job

--- migrate/migrate.js
+++ migrate/migrate.js
@@ -2,7 +2,7 @@
  * @author nttdocomo
  */
 var http = require("http"), mysql = require('mysql'),
-pool  = require('../pool'),
+pool  = require('../crawler/transfermarkt.co.uk/pool'),
 sql = "INSERT INTO `player2position`(player_id,position_id) SELECT transfermarket_player.player_ref_id,position.id FROM `position` JOIN `transfermarket_player` ON transfermarket_player.position = position.name WHERE CONCAT(transfermarket_player.player_ref_id,'-',position.id) NOT IN (SELECT CONCAT(player_id,'-',position_id) FROM `player2position`) AND transfermarket_player.player_ref_id != 0";
 pool.getConnection(function(err, connection) {
 	connection.query(sql, function(err,result) {
