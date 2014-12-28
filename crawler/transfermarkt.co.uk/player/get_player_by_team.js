15309ee7a69d5fa725a9ccf9e2caf1d115419734
unknown <nttdocomo@.(none)>
2014-12-28 16:58:27 +0800

    update util path

--- crawler/transfermarkt.co.uk/player/get_player_by_team.js
+++ crawler/transfermarkt.co.uk/player/get_player_by_team.js
@@ -1,10 +1,15 @@
 /**
  * @author nttdocomo
  */
-var http = require("http"), cheerio = require('cheerio'),connection = require("../db"), mysql = require('mysql'),StringDecoder = require('string_decoder').StringDecoder,Team = require('../team/model'),
-Player = require('./model'),moment = require('moment'),
-trim = require('../utils').trim,
-excute  = require('../excute'),
+var http = require("http"),
+cheerio = require('cheerio'),
+mysql = require('mysql'),
+moment = require('moment'),
+StringDecoder = require('string_decoder').StringDecoder,
+connection = require("../../../db"),
+Team = require('../team/model'),
+Player = require('./model'),
+excute  = require('../../../excute'),
 Crawler = require("simplecrawler"),
 host = 'http://www.transfermarkt.co.uk',
 crawler = new Crawler('www.transfermarkt.co.uk');
@@ -13,9 +18,9 @@ crawler.interval = 500;
 crawler.timeout = 5000;
 crawler.discoverResources = false;
 crawler.customHeaders = {
-	Cookie:'__qca=P0-912270038-1403184571295; 22ea10c3df12eecbacbf5e855c1fc2b3=4b2f77b042760e0b6c4403263173b81a02199e1da%3A4%3A%7Bi%3A0%3Bs%3A6%3A%22561326%22%3Bi%3A1%3Bs%3A9%3A%22nttdocomo%22%3Bi%3A2%3Bi%3A31536000%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D; POPUPCHECK=1406040912765; PHPSESSID=kjuus3jlq0md5vhhq0hn2p7571; __utma=1.264986923.1403184483.1406010530.1406012399.139; __utmb=1.1.10.1406012399; __utmc=1; __utmz=1.1405646456.117.3.utmcsr=transfermarkt.com|utmccn=(referral)|utmcmd=referral|utmcct=/wettbewerbe/national/wettbewerbe/26'
+	Cookie:'POPUPCHECK=1419841166132; __utma=1.692075973.1419751813.1419751813.1419754691.2; __utmb=1.3.10.1419754691; __utmc=1; __utmz=1.1419751813.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); TMSESSID=j0r48dldbo63rq7ohalpne9486; 22ea10c3df12eecbacbf5e855c1fc2b3=a8929849dfc1e9967b757427b43d5fdba30c368fa%3A4%3A%7Bi%3A0%3Bs%3A6%3A%22561326%22%3Bi%3A1%3Bs%3A9%3A%22nttdocomo%22%3Bi%3A2%3Bi%3A31536000%3Bi%3A3%3Ba%3A0%3A%7B%7D%7D; _mcreg=1'
 };
-crawler.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36';
+crawler.userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36';
 var transfers = [],
 update_transfers = [];
 crawler.on("fetchcomplete",function(queueItem, responseBuffer, response){
