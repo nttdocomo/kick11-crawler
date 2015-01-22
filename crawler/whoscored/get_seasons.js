45d2d4d4c620e56ff04c9403c34144c77d060c05
unknown <nttdocomo@.(none)>
2015-01-22 17:46:36 +0800

    shit add season to tourment

--- crawler/whoscored/get_seasons.js
+++ crawler/whoscored/get_seasons.js
@@ -6,7 +6,7 @@ module.exports = function(stages,fn){
     stages.forEach(function(stage){
         var season_id = stage[6];
         if(!fn(season_id)){
-            excute(mysql.format('INSERT INTO whoscored_tournaments SET ?',{
+            excute(mysql.format('INSERT INTO whoscored_seasons SET ?',{
 	        	id:season_id
 	        }));
         }
