ad2ed7c507adedf405f0e9807d67aa36893c07ff
unknown <nttdocomo@.(none)>
2015-08-18 23:18:25 +0800

    add new files and delete unused files

--- crawler/whoscored/season.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author nttdocomo
- */
-var excute = require('../../promiseExcute'),
-mysql = require('mysql');
-module.exports.get_season_by_id = function(id){
-    return excute(mysql.format('SELECT 1 FROM whoscored_seasons WHERE id = ?',[id]));
-};
---
+++ model/whoscored/season.js
@@ -0,0 +1,42 @@
+/**
+ * @author nttdocomo
+ */
+var excute = require('../../promiseExcute'),
+mysql = require('mysql'),
+_ = require('underscore'),
+difference = require('../../utils').difference,
+Model = require('../../model'),
+Season = Model.extend({
+    tableName:'whoscored_seasons',
+    is_exist:function(){
+        return excute(mysql.format('SELECT 1 FROM '+this.constructor.table+' WHERE id = ?',[this.get('id')])).then(function(row){
+            return row.length;
+        })
+    },
+    needToUpdate:function(data,row){
+        this._super(data,row);
+        var diff;
+        if(!_.isEqual(data,row)){
+            diff = difference(row,data);
+            return diff;
+            //return excute(mysql.format('UPDATE `transfermarket_team` SET ? WHERE id = ?',[diff,id]))
+        }
+        return false;
+    }
+});
+Season.excute = excute;
+Season.table = 'whoscored_seasons';
+Season.get_by_id = function(id){
+    return excute(mysql.format('SELECT 1 FROM '+this.table+' WHERE id = ?',[id]))
+};
+Season.get_seasons = function(seasons){
+    return seasons.reduce(function(sequence, item){
+        var season = new Season({
+            id:item[6]
+        })
+        return sequence.then(function(){
+            return season.save();
+        });
+    },Promise.resolve())
+};
+module.exports = Season;
