64d9c005cff8e51ff6805964a8272aedf28f0def
nttdocomo <nttdocomo.ouyi@gmail.com>
2015-12-21 18:17:50 +0800

    UPDATE

--- crawler/whoscored/getMatchCentrePlayerStatistics.js
+++ crawler/whoscored/getMatchCentrePlayerStatistics.js
@@ -19,20 +19,42 @@ condition = 0,
 now = moment.utc(),
 clone = now.clone();
 migrate_match().then(migrate_match_player_statistics).then(function(){
-	return excute('SELECT * FROM `whoscored_match` WHERE id IN (SELECT whoscored_match_id FROM `whoscored_match_match` WHERE whoscored_match_id NOT IN (SELECT matchId FROM `whoscored_match_player_statistics`))')
+	return excute('SELECT id,team1_id,team2_id FROM `whoscored_match` WHERE id IN (SELECT whoscored_match_id FROM `whoscored_match_match`)')
 }).then(function(row){
+	return row.reduce(function(sequence, match,i){
+		return excute(mysql.format('SELECT 1 FROM `whoscored_match_player_statistics` WHERE teamId = ? AND matchId = ? LIMIT 1',[match.team1_id,match.id])).then(function(stats){
+    		if(!stats.length){
+    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+match.team1_id+'&matchId='+match.id);
+    		}
+    		return Promise.resolve();
+    	}).then(function(){
+    		return excute(mysql.format('SELECT 1 FROM `whoscored_match_player_statistics` WHERE teamId = ? AND matchId = ? LIMIT 1',[match.team2_id,match.id]))
+    	}).then(function(stats){
+    		if(!stats.length){
+    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+row[0].team2_id+'&matchId='+row[0].id);
+    		}
+    		return Promise.resolve();
+    	})
+	},Promise.resolve())
 	//crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=passing&statsAccumulationType=0&teamIds=24&matchId=959591');
     if(teamId && matchId){
         crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+teamId+'&matchId='+matchId);
+    	return Promise.resolve();
     } else {
-    	if(row.length){
-    		row.forEach(function(item){
-    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+item.team1_id+'&matchId='+item.id);
-    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+item.team2_id+'&matchId='+item.id);
-    		})
-    	}
+    	return excute(mysql.format('SELECT 1 FROM `whoscored_match_player_statistics` WHERE teamId = ? AND matchId = ? LIMIT 1',[row[0].team1_id,row[0].id])).then(function(stats){
+    		if(!stats.length){
+    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+row[0].team1_id+'&matchId='+row[0].id);
+    		}
+    		return Promise.resolve();
+    	}).then(function(){
+    		return excute(mysql.format('SELECT 1 FROM `whoscored_match_player_statistics` WHERE teamId = ? AND matchId = ? LIMIT 1',[row[0].team2_id,row[0].id]))
+    	}).then(function(stats){
+    		if(!stats.length){
+    			crawler.queueURL(host + '/StatisticsFeed/1/GetMatchCentrePlayerStatistics?category=summary&subcategory=all&statsAccumulationType=0&isCurrent=true&teamIds='+row[0].team2_id+'&matchId='+row[0].id);
+    		}
+    		return Promise.resolve();
+    	})
     }
-    return Promise.resolve();
 }).then(function(){
     crawler.start();
 }).catch(function(err){
