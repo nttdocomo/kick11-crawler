967d285dc6c73323b41b419c8010f1c2c8fe6aee
nttdocomo <nttdocomo.ouyi@gmail.com>
2016-03-03 14:01:07 +0800

    migrate_match_player_statistics cause the out of memery, add more query to decrease the match result

--- crawler/whoscored/migrate_match.js
+++ crawler/whoscored/migrate_match.js
@@ -1,16 +1,13 @@
-var excute = require('../../promiseExcute'),
-mysql = require('mysql'),
+var mysql = require('mysql'),
+excute = require('../../promiseExcute'),
 Match = require('../../model/whoscored/matches');
 module.exports = function(){
     return excute('SELECT whoscored_team_id FROM whoscored_team_team').then(function(row){
-        console.log('whoscored_team length:' + row.length)
         var team_id = row.map(function(item,i){
             return item.whoscored_team_id
         })
-        console.log('team_id length:' + team_id.length)
         return excute(mysql.format('SELECT * FROM `whoscored_match` WHERE id NOT IN (SELECT whoscored_match_id FROM `whoscored_match_match`) AND team1_id IN (?) AND team2_id IN (?)',[team_id,team_id]))
     }).then(function(whoscored_matches){
-        console.log('whoscored_match length:' + whoscored_matches.length)
         if(whoscored_matches.length){
             return whoscored_matches.reduce(function(sequence,match,i){
                 return sequence.then(function(){
--- crawler/whoscored/migrate_match_player_statistics.js
+++ crawler/whoscored/migrate_match_player_statistics.js
@@ -1,7 +1,25 @@
-var excute = require('../../promiseExcute'),
+var mysql = require('mysql'),
+excute = require('../../promiseExcute'),
 MatchPlayerStatistic = require('../../model/kick11/statistic');
 module.exports = function(){
-    return excute('SELECT * FROM `whoscored_match_player_statistics` WHERE id NOT IN (SELECT whoscored_match_player_statistics_id FROM `whoscored_match_player_statistics_relation`)').then(function(whoscored_match_player_statistics){
+    var whoscored_match_id,whoscored_team_id,whoscored_player_id;
+    return excute('SELECT whoscored_match_id FROM whoscored_match_match').then(function(row){
+        whoscored_match_id = row.map(function(item){
+            return item.whoscored_match_id
+        })
+        return excute('SELECT whoscored_team_id FROM whoscored_team_team')
+    }).then(function(row){
+        whoscored_team_id = row.map(function(item){
+            return item.whoscored_team_id
+        })
+        return excute('SELECT whoscored_player_id FROM whoscored_player_player')
+    }).then(function(row){
+        whoscored_player_id = row.map(function(item){
+            return item.whoscored_player_id
+        })
+        return excute(mysql.format('SELECT * FROM `whoscored_match_player_statistics` WHERE id NOT IN (SELECT whoscored_match_player_statistics_id FROM `whoscored_match_player_statistics_relation`) AND playerId IN (?) AND teamId IN (?) AND matchId IN (?)',[whoscored_player_id,whoscored_team_id,whoscored_match_id]))
+    }).then(function(whoscored_match_player_statistics){
+        console.log('whoscored_match_player_statistics length:' + whoscored_match_player_statistics.length)
         if(whoscored_match_player_statistics.length){
             console.log('migrate match_player_statistics')
             return whoscored_match_player_statistics.reduce(function(sequence,whoscored_match_player_statistic){
